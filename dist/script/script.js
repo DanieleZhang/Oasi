b36be3f753ce2d76099cef8d8b95764a22106550
alu0101225845 <alu0101225845@ull.edu.es>
2023-07-19 22:22:54 +0200

    nav init, should modify color and hidden menu, and write css in css files

---
+++ dist/script/script.js
@@ -0,0 +1,18 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+    slides.forEach((slide, index) => {
+        slide.classList.remove('active');
+        if (index === slideIndex) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Change slide every 3 seconds (adjust timing as needed)
